d9375d82c532f60721738282fa91930c8349baef
Ruben Winkler <r.winkler1412@gmail.com>
2018-04-23 21:50:01 +0200

    Worked!
    - Added ACF for all pages

--- single-kurse.php
+++ single-kurse.php
@@ -4,20 +4,33 @@
 
 <!-- KURSE -->
 
+<?php
+
+$id = get_the_ID();
+
+//Advanced Custom Fields
+$kurstitel          =   get_field("kurstitel");
+$kursuntertitel     =   get_field("kursuntertitel");
+$kursbild           =   get_field("kursbild");
+$teasertext         =   get_field("teasertext");
+$kursbeschreibung   =   get_field("kursbeschreibung");
+$playlist           =   get_field("playlist");
+$angebotspreis      =   get_field("angebotspreis");
+$standardpreis      =   get_field("standardpreis");
+$gutscheincode      =   get_field("gutscheincode");
+$gutscheincode_url  =   get_field("gutscheincode_url");
+$kurslevel          =   get_field("kurslevel");
+$videozahl          =   get_field("videozahl");
+$kurslaenge         =   get_field("kurslaenge");
+$teilnehmerzahl     =   get_field("teilnehmerzahl");
+$bewertung          =   get_field("bewertung");
+$anforderungen      =   get_field("anforderungen");
+$lernziele          =   get_field("lernziele");
+$zielgruppe         =   get_field("zielgruppe");
+
+?>
+
 <section>
-
-        <?php
-
-        $id = get_the_ID();
-
-        //Advanced Custom Fields
-        $kurstitel        =   get_field("kurstitel");
-        $kursuntertitel   =   get_field("kursuntertitel");
-        $kursbild         =   get_field("kursbild");
-        $teasertext       =   get_field("teasertext");
-        $kursbeschreibung       =   get_field("kursbeschreibung");
-
-        ?>
 
   <div class="container spacer-md-top spacer-xl-bottom">
     <div class="row">
@@ -34,11 +47,13 @@
     </div>
     <div class="row">
       <div class="col-lg-8 col-md-12">
+        <?php if ( !empty($playlist) ): ?>
         <div class="course-video-container spacer-sm-bottom">
           <div class="course-video">
-            <iframe src="https://www.youtube.com/embed/9PJ7r9tjS_s" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <iframe src="<?php echo $playlist; ?>" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
           </div>
         </div>
+        <?php endif; ?>
         <div class="course-description">
         <h2>Kursbeschreibung</h2>
         <?php if ( !empty($kursbeschreibung) ): ?>
@@ -50,8 +65,20 @@
         
         <article class="offer-area hidden-md">
           <h2>Aktuelles Angebot</h2>
-          <div class="current-offer-text"><div><span>9,99€</span> statt 199,99€</div><br> mit dem Gutscheincode:</div>
-          <a href="" class="btn-typ-3 voucher-code-btn">"WEBDESIGN"</a>
+          <div class="current-offer-text">
+            <div>
+              <?php if ( !empty($angebotspreis) ): ?>
+              <span><?php echo $angebotspreis; ?></span>
+              <?php endif; ?>
+              <?php if ( !empty($standardpreis) ): ?>
+              statt <?php echo $standardpreis; ?>
+              <?php endif; ?>
+            </div>
+            <br> mit dem Gutscheincode:
+          </div>
+          <?php if ( !empty($gutscheincode_url) && !empty($gutscheincode) ): ?>
+          <a href="<?php echo $gutscheincode_url; ?>" class="btn-typ-3 voucher-code-btn" target="_blank">"<?php echo $gutscheincode; ?>"</a>
+          <?php endif; ?>
           <div class="subtext">Hinweis: Der Kurs wird über Udemy.com bereitgestellt. Der Gutschein-Code gilt ausschließlich auf Udemy.com.</div>
         </article>
         <article>
@@ -60,31 +87,41 @@
             <span class="feature-icon">
               <i class="fas fa-angle-double-up"></i>
             </span>
-            alle Level
+            <?php if ( !empty($kurslevel) ): ?>
+            <?php echo $kurslevel; ?>
+            <?php endif; ?>
           </div>
           <div class="course-feature">
             <span class="feature-icon">
               <i class="fas fa-video"></i>
             </span>
-            193 Video-Lektionen
+            <?php if ( !empty($videozahl) ): ?>
+            <?php echo $videozahl; ?>
+            <?php endif; ?> Video-Lektionen
           </div>
           <div class="course-feature">
             <span class="feature-icon">
               <i class="fas fa-clock"></i>
             </span>
-            32h+ Videomaterial
+            <?php if ( !empty($kurslaenge) ): ?>
+            <?php echo $kurslaenge; ?>
+            <?php endif; ?> Videomaterial
           </div>
           <div class="course-feature">
             <span class="feature-icon">
               <i class="fas fa-users"></i>
             </span>
-            400+ Studenten
+            <?php if ( !empty($teilnehmerzahl) ): ?>
+            <?php echo $teilnehmerzahl; ?>
+            <?php endif; ?> Studenten
           </div>
           <div class="course-feature">
             <span class="feature-icon">
               <i class="fas fa-star"></i>
             </span>
-            4,9/5 Sternen
+            <?php if ( !empty($bewertung) ): ?>
+            <?php echo $bewertung; ?>
+            <?php endif; ?> Sternen
           </div>
           <div class="course-feature">
             <span class="feature-icon">
@@ -97,41 +134,46 @@
         <article>
           <h2>Anforderungen</h2>
           <ul>
-            <li>Ein PC oder Laptop (Windows, Mac oder Linux)</li>
-            <li>Alle notwendigen Tools sind zu 100% kostenlos und werden im Laufe des Kurses installiert</li>
-            <li>Es sind keine Programmier-Kenntnisse notwendig</li>
-            <li>Es sind keine Englisch-Kenntnisse notwendig</li>
-            <li>Lernbegeisterung und Neugier sind alles was du für diesen Kurs brauchst</li>
+          <?php if ( !empty($anforderungen) ): ?>
+          <?php echo $anforderungen; ?>
+          <?php endif; ?>
           </ul>
         </article>
 
         <article>
           <h2>Lernziele</h2>
           <ul>
-            <li>Lerne alles zum Thema HTML5 und CSS3 in Theorie und Praxis</li>
-            <li>Setze ein aufwändiges und umfangreiches Praxis-Projekt um</li>
-            <li>Lerne responsive und mobile-first Webdesign</li>
-            <li>Baue ein eigenes Grid-System für responsive Layouts</li>
-            <li>Plane strukturierte Webprojekte, wie in der Praxis</li>
-            <li>Starte deine Karriere als Webdesigner oder Webentwickler</li>
+          <?php if ( !empty($lernziele) ): ?>
+          <?php echo $lernziele; ?>
+          <?php endif; ?>
           </ul>
         </article>
 
         <article>
           <h2>Zielgruppe</h2>
           <ul>
-            <li>Alle, die einen detaillierten und ausführlichen Kurs zum Thema HTML und CSS suchen</li>
-            <li>Alle, die für Beruf, Praktikum oder Studium HTML und CSS lernen wollen</li>
-            <li>Alle, die ihr Wissen über HTML und CSS auffrischen oder verbessern wollen</li>
-            <li>Alle, die Webdesigner oder Webentwickler werden wollen</li>
-            <li>Alle, die mit einer weiteren Qualifikation im Lebenslauf glänzen möchten</li>
+          <?php if ( !empty($zielgruppe) ): ?>
+          <?php echo $zielgruppe; ?>
+          <?php endif; ?>
           </ul>
         </article>
 
         <article class="offer-area hidden-lg">
-          <h2>Aktuelles Angebot</h2>
-          <div class="current-offer-text"><div><span>9,99€</span> statt 199,99€</div><br> mit dem Gutscheincode:</div>
-          <a href="" class="btn-typ-3 voucher-code-btn">"WEBDESIGN"</a>
+        <h2>Aktuelles Angebot</h2>
+          <div class="current-offer-text">
+            <div>
+              <?php if ( !empty($angebotspreis) ): ?>
+              <span><?php echo $angebotspreis; ?></span>
+              <?php endif; ?>
+              <?php if ( !empty($standardpreis) ): ?>
+              statt <?php echo $standardpreis; ?>
+              <?php endif; ?>
+            </div>
+            <br> mit dem Gutscheincode:
+          </div>
+          <?php if ( !empty($gutscheincode_url) && !empty($gutscheincode) ): ?>
+          <a href="<?php echo $gutscheincode_url; ?>" class="btn-typ-3 voucher-code-btn">"<?php echo $gutscheincode; ?>"</a>
+          <?php endif; ?>
           <div class="subtext">Hinweis: Der Kurs wird über Udemy.com bereitgestellt. Der Gutschein-Code gilt ausschließlich auf Udemy.com.</div>
         </article>
 
